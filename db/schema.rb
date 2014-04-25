d699bb331d18dad76740e5651ac98cd98f93f578
dawidspamer <dawidspamer@gmail.com>
2014-04-25 11:55:40 -0700

    Removing useless columns from Users Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424222244) do
+ActiveRecord::Schema.define(version: 20140425184856) do
 
   create_table "rooms", force: true do |t|
     t.string   "name"
@@ -37,8 +37,6 @@ ActiveRecord::Schema.define(version: 20140424222244) do
     t.datetime "updated_at"
     t.integer  "soundcloud_user_id"
     t.string   "soundcloud_username"
-    t.string   "soundcloud_refresh_token"
-    t.datetime "soundcloud_expired_at"
   end
 
 end
