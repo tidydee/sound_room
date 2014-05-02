824f98492a31306770d643346bdc823e1ac5c0fe
dawidspamer <dawidspamer@gmail.com>
2014-05-01 17:07:25 -0700

    Adding SCSongId to DB with AJAX, NO ERRORS

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,34 +1,18 @@
 class SongsController < ApplicationController
 
-  # def index
-  #   @song = Song.all
-  # end
-
-  # def create
-  #   # @room = Room.all
-  #   @song = Song.new(song_params)
-  #   @song.user_id = current_user.id if current_user
-
-
-  #   if @song.save
-  #     flash[:success] = "Song Successfully Added!"
-  #     redirect_to :root
-  #     # redirect_to users_path
-  #   else
-  #     flash[:success] = "Something's Fishy - Song Was NOT Added!"
-  #     render "rooms/show"
-  #   end
-  # end
-
-  
+  def index
+    @songs = Song.all
+    @song = Song.new
+  end
 
   def create
+    # @song = Song.new(params[:song])
     @song = Song.new({ soundcloud_track_id: params[:soundcloud_track_id] })
     # binding.pry
-    # # @room.user_id = current_user.id if current_user
+    # @room.user_id = current_user.id if current_user
     respond_to do |format|
       if @song.save
-        format.html { redirect_to @song, notice: "Save process completed!" }
+        # format.html { redirect_to :root, notice: "Save process completed!" }
         format.json { render json: @song, status: :created, location: @song }
       else
         format.html { 
@@ -39,13 +23,6 @@ class SongsController < ApplicationController
       end
     end
   end
-
-
-
-
-
-
-
 
   protected
 
