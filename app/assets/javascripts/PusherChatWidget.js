48e2a7dd04032b9383c0f3c4022820fce70c8d36
Melissa Hubert <meloizabeth@live.com>
2014-05-02 04:23:00 +0000

    changes to some looks

--- app/assets/javascripts/PusherChatWidget.js
+++ app/assets/javascripts/PusherChatWidget.js
@@ -15,7 +15,7 @@ function PusherChatWidget(pusher, options) {
   options = options || {};
   this.settings = $.extend({
     maxItems: 50, // max items to show in the UI. Items beyond this limit will be removed as new ones come in.
-    chatEndPoint: 'ruby-sinatra/ruby.rb', // the end point where chat messages should be sanitized and then triggered
+    chatEndPoint: 'ruby-sinatra/chat.rb', // the end point where chat messages should be sanitized and then triggered
     channelName: document.location.href, // the name of the channel the chat will take place on
     appendTo: document.body, // A jQuery selector or object. Defines where the element should be appended to
     debug: true
