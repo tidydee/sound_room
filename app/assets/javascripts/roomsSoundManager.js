02904b3b16aa76cf5cebac3b7d5393080fa605e6
amohbata <amohbata@gmail.com>
2014-05-08 21:10:16 +0000

    first commit

--- app/assets/javascripts/roomsSoundManager.js
+++ app/assets/javascripts/roomsSoundManager.js
@@ -1,75 +1,84 @@
 var currentTrackId = undefined;
 var currentPlaying = false;
 var sound = undefined;
+
 function playTrack(id) {
-  // $('.track').addClass('unselected-songs');
   console.log('sound playing');
-    SC.whenStreamingReady(function() {
-      if (id === currentTrackId) {
-        if (currentPlaying) {
-          sound.pause();
-        } else {
-          sound.play();
-        }
-        currentPlaying = !currentPlaying;
+  SC.whenStreamingReady(function() {
+    // currentTrackId = scId of previously playing song
+    // currentPlaying is true if sound is playing
+    if (id === currentTrackId) {
+      if (currentPlaying) {
+        sound.pause();
       } else {
-        if (sound) {
-          $('.tracks[data-sc-track-id='+currentTrackId+']').removeClass("selected-song");
-          sound.stop();
-        }
-        currentTrackId = id
-        
-        sound = SC.stream(id, { autoPlay: false }, function(sound){
-          console.log(sound);
-          currentPlaying = true;
-      
-          sound.play({
-            onfinish: function(){
-              console.log('track_finished');
-              var playingTrackElement = $('.tracks[data-sc-track-id='+id+']');
-              playingTrackElement.addClass("selected-song");
-              skipper();
-            },
-            onplay: function(){
-            console.log('inside onplay');
-            var playingTrackElement = $('.tracks[data-sc-track-id='+id+']');
-            playingTrackElement.addClass("selected-song");
-            console.log(playingTrackElement);
-            },
-            whileplaying: function() {
-              $(".progressBar").css('width', ((this.position/this.duration) * 100) + '%');
-            },
-          });
-        });
+        sound.play();
       }
-
-      function skipper(){
+      currentPlaying = !currentPlaying;
+    } else {
+      if (sound) {
+        $('.tracks[data-sc-track-id='+currentTrackId+']').removeClass("selected-song");
         sound.stop();
-        var nextIndex = window.playlist.indexOf(id) + 1
-        var nextTrackId;
-        if (nextIndex <= window.playlist.length - 1) {
-          nextTrackId = window.playlist[nextIndex];
-        }else{
-          nextTrackId = window.playlist[0];
-        }
-        playTrack(nextTrackId);
       }
-      $('#skip').click(skipper);
-
-      $('#pause').click(function(e){
-          sound.pause();
-      });
-      $('#play').click(function(e){
-          sound.resume();
-      });
-      $('#mute').click(function(e){
-          sound.setVolume(0);
-      });
-      $('#med-volume').click(function(e){
-          sound.setVolume(40);
-      });
-      $('#high-volume').click(function(e){
-          sound.setVolume(100);
-      });
-    });
+      currentTrackId = id;
+      handleSound();  
+    }
   }
+} 
+
+
+
+function handleSound(){
+  sound = SC.stream(currentTrackId, { autoPlay: false }, function(sound){
+    console.log(sound);
+    currentPlaying = true;
+
+    sound.play({
+      onfinish: function(){
+        console.log('track_finished');
+        var playingTrackElement = $('.tracks[data-sc-track-id='+id+']');
+        playingTrackElement.addClass("selected-song");
+        skipper(sound);
+      },
+      onplay: function(){
+        console.log('inside onplay');
+        var playingTrackElement = $('.tracks[data-sc-track-id='+id+']');
+        playingTrackElement.addClass("selected-song");
+        console.log(playingTrackElement);
+      },
+      whileplaying: function() {
+        $(".progressBar").css('width', ((this.position/this.duration) * 100) + '%');
+      }
+    });
+  });
+}
+
+function skipper(){
+  sound.stop();
+  var nextIndex = window.playlist.indexOf(id) + 1
+  var nextTrackId;
+  if (nextIndex <= window.playlist.length - 1) {
+    nextTrackId = window.playlist[nextIndex];
+  }else{
+    nextTrackId = window.playlist[0];
+  }
+  playTrack(nextTrackId);
+}
+$('#skip').click(skipper);
+
+$('#pause').click(function(e){
+    sound.pause();
+});
+$('#play').click(function(e){
+    sound.resume();
+});
+$('#mute').click(function(e){
+    sound.setVolume(0);
+});
+$('#med-volume').click(function(e){
+    sound.setVolume(40);
+});
+$('#high-volume').click(function(e){
+    sound.setVolume(100);
+});
+  //   });
+  // }
