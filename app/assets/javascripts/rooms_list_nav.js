0d036c2b3dc1fed6d9919815f528a02046a28fc2
dawidspamer <dawidspamer@gmail.com>
2014-05-08 20:48:53 -0700

    fixing rooms list on popup

--- app/assets/javascripts/rooms_list_nav.js
+++ app/assets/javascripts/rooms_list_nav.js
@@ -1,10 +1,11 @@
 $('.join-room-btn').on("click", function() {
   console.log('JOIN ROOMS BUT!!!');
+  // event.preventDefault();
 
-  if($('.rooms-list-nav').hasClass("hide-class")){
-    $('.rooms-list-nav').removeClass("hide-class");
+  if($('.rooms-list').hasClass("hide-class")){
+    $('.rooms-list').removeClass("hide-class");
   }else{
-    $('.rooms-list-nav').addClass("hide-class");
+    $('.rooms-list').addClass("hide-class");
   }
 
 });
