3dcc63d0634a9ac33f1a9a4179eee3acfcb6c94f
dawidspamer <dawidspamer@gmail.com>
2014-05-12 13:42:24 -0700

    Reverting back to Ali CodeSetup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider "soundcloud", "d8eeb9848b095ce9d0e77eeb9d6f360a", "11c7bc108ece108b7be66575583486b2"
+  provider "soundcloud", "173c1924dc4e55b0db3b89a45bb71561", "11c7bc108ece108b7be66575583486b2"
 end
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
