5ab9f334272b1ead6cada109164d10851806633b
dawidspamer <dawidspamer@gmail.com>
2014-05-14 16:58:32 -0700

    Refactoring Users/Index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,14 @@
 Rails.application.routes.draw do
   
   root to: 'rooms#index'
-
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/soundcloud', as: 'soundcloud_session'
   get '/auth/logout',  to: 'sessions#logout'
   post '/pusher', to: 'pusher#create'
   get '/pusher', to: 'pusher#show'
 
-  resources :users
-  
+  resources :users, only: [:index]
   resources :songs
-
-  # resources :rooms, only: [:index, :create, :show, :destroy]
-  
   resources :rooms
-  # do
-  #   resources :songs, only: [:index, :create, :destroy]
-  # end
-
   resource :session, only: [:new, :create, :destroy]
-
-  end
+end
