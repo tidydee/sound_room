0314f91acedb0c0c708767bee542f749aced3c35
dawidspamer <dawidspamer@gmail.com>
2014-06-02 16:29:54 -0700

    Sanitizing code for Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider "soundcloud", "d8eeb9848b095ce9d0e77eeb9d6f360a", "3f6c773c7605625ea6e4a3b264688261"
+  provider "soundcloud", ENV["SOUNDCLOUD_CLIENT_ID"], ENV["SOUNDCLOUD_SECRET"]
 end
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
